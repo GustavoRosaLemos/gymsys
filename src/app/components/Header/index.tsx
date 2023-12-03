1cee2d9d8957b838055c4200aef7b11b384a8369
Gustavo Rosa Lemos <dev.gustavolemos@outlook.com>
2023-12-03 20:38:14 -0300

    Added server and basic header

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Button, Grid, Group, Paper, Text } from "@mantine/core";
+
+export default function Header() {
+    return (
+        <Paper shadow="xl" p="xl">
+            <Group>
+                <Text fw="bold" size="xl" c="gray">GYMSYS</Text>
+                <Group>
+                    ...
+                </Group>
+            </Group>
+        </Paper>
+    )
+}
